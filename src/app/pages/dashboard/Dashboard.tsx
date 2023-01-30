eaf258dc0af489b77d465b51e67da25cc324e079
Diego Bittencourt <diegohevant@gmail.com>
2023-01-29 22:10:50 -0300

    Fazendo a navegação

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,11 @@
+import {Link} from 'react-router-dom'
+
 export const Dashboard = () => {
-    return(
-        <h1>Dashboard</h1>
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Link to={"/entrar"}>login</Link>
+        </div>
+
     )
 }
